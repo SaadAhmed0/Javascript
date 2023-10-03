e0187d48a3bd9dd67b22a01f6b2dc2d5e40a4628
Saad Ahmed Siddiqui <62701144+SaadAhmed0@users.noreply.github.com>
2023-10-03 11:40:20 +0000

    new changes

--- .devcontainer/01basics/03_Conversion.js
+++ .devcontainer/01basics/03_Conversion.js
@@ -3,8 +3,8 @@ let score = null
 console.log(typeof(score));
 let valueInNumber = Number(score)
 
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33ab" => NaN ; not a number
@@ -13,9 +13,27 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
 
-// 1=> true
+// 1=> trueconsole.log(booleanIsLoggedIn)
 // 0=> false
 // ""=> false
 // "saaa" => true
+
+let word = 44
+// console.log(string)
+// let isstring = string(word)
+
+// *********************** Operations ***********************
+
+let value = 3;
+let negvalue = -value
+// console.log(negvalue);
+// console.log(typeof(negvalue))
+
+let str1 = "Hello"
+let str2 = " Saad"
+let str3= str1+str2
+
+
+console.log(3 + 3+"3")
+console.log()
